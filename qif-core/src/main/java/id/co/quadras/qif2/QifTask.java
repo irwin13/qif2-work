7ce663574197b326ee12cff7a4c28e2d34f9abac
irwin <muhammad.irwin@quadras.co.id>
2014-04-10 11:31:33 +0700

    first classes

---
+++ qif-core/src/main/java/id/co/quadras/qif2/QifTask.java
@@ -0,0 +1,7 @@
+package id.co.quadras.qif2;
+
+/**
+ * @author irwin Timestamp : 07/04/2014 11:53
+ */
+public class QifTask {
+}
---
+++ qif-core/src/main/java/id/co/quadras/qif2/model/entity/QifEvent.java
@@ -0,0 +1,8 @@
+package id.co.quadras.qif2.model.entity;
+
+/**
+ * @author irwin Timestamp : 07/04/2014 11:53
+ */
+public class QifEvent {
+
+}
