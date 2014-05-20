999b7905e11ea8f1216a9b0bc8c4c5213dd41ea3
irwin <muhammad.irwin@quadras.co.id>
2014-05-20 19:03:37 +0700

    update liquibase, execute them not through maven

--- qif-dev/src/main/java/id/co/quadras/qif/dev/web/AppStatus.java
+++ qif-dev/src/main/java/id/co/quadras/qif/dev/web/AppStatus.java
@@ -1,5 +1,11 @@
 package id.co.quadras.qif.dev.web;
 
+import com.irwin13.winwork.basic.WinWorkConstants;
+import com.irwin13.winwork.basic.config.WinWorkConfig;
+import com.irwin13.winwork.basic.utilities.WinWorkUtil;
+import id.co.quadras.qif.dev.QifDevContextListener;
+import id.co.quadras.qif.dev.guice.GuiceFactory;
+
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -12,7 +18,13 @@ import java.io.IOException;
 public class AppStatus extends HttpServlet {
 
     @Override
-    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+            throws ServletException, IOException {
 
+        WinWorkConfig winWorkConfig = GuiceFactory.getInjector().getInstance(WinWorkConfig.class);
+        String htmlContent = WinWorkUtil.appStatusView(winWorkConfig, QifDevContextListener.START,
+                WinWorkConstants.SDF_FULL);
+
+        resp.getWriter().append(htmlContent);
     }
 }
