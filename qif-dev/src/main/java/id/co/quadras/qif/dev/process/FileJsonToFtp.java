b194ff50a1e3861d203fd8145d90f9e58039196b
irwin <irwin13@quadras.co.id>
2014-05-28 19:05:00 +0700

    add TaskRepository

--- qif-dev/src/main/java/id/co/quadras/qif/dev/process/FileJsonToFtp.java
+++ qif-dev/src/main/java/id/co/quadras/qif/dev/process/FileJsonToFtp.java
@@ -4,6 +4,7 @@ import id.co.quadras.qif.QifTaskMessage;
 import id.co.quadras.qif.dev.guice.GuiceFactory;
 import id.co.quadras.qif.dev.task.JsonToXml;
 import id.co.quadras.qif.dev.task.PutToFtp;
+import id.co.quadras.qif.helper.TaskRepository;
 import id.co.quadras.qif.model.vo.QifActivityResult;
 
 /**
@@ -13,10 +14,10 @@ public class FileJsonToFtp extends FileProcess {
 
     @Override
     protected QifActivityResult implementProcess(Object processInput) {
-        JsonToXml jsonToXml = GuiceFactory.getInjector().getInstance(JsonToXml.class);
+        JsonToXml jsonToXml = TaskRepository.getTask(GuiceFactory.getInjector(), JsonToXml.class);
         Object resultXml = jsonToXml.executeTask(new QifTaskMessage(this, processInput));
 
-        PutToFtp putToFtp = GuiceFactory.getInjector().getInstance(PutToFtp.class);
+        PutToFtp putToFtp = TaskRepository.getTask(GuiceFactory.getInjector(), PutToFtp.class);
         putToFtp.executeTask(new QifTaskMessage(this, resultXml));
 
         return new QifActivityResult(SUCCESS, null, null);
