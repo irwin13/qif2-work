1ee992c93de62b45e056d9fee78f3496231edf59
irwin <irwin13@quadras.co.id>
2014-06-02 23:26:24 +0700

    add clean-data.sql

---
+++ qif-dev/db/clean-data.sql
@@ -0,0 +1,16 @@
+delete from qif_activity_log;
+delete from qif_activity_log_data;
+delete from qif_activity_log_input_msg;
+delete from qif_activity_log_output_msg;
+
+delete from qif_event_log;
+delete from qif_event_log_msg;
+
+select * from qif_activity_log;
+select * from qif_activity_log_data;
+select * from qif_activity_log_input_msg;
+select * from qif_activity_log_output_msg;
+
+select * from qif_event_log;
+select * from qif_event_log_msg;
+
