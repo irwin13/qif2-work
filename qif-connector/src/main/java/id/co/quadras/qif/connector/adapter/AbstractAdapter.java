33c23a65aada9a6811bcad099c85f947a8231e1c
irwin <irwin13@gmail.com>
2014-07-18 17:12:15 +0700

    add null safety

--- qif-connector/src/main/java/id/co/quadras/qif/connector/adapter/AbstractAdapter.java
+++ qif-connector/src/main/java/id/co/quadras/qif/connector/adapter/AbstractAdapter.java
@@ -1,10 +1,8 @@
 package id.co.quadras.qif.connector.adapter;
 
-import id.co.quadras.qif.core.exception.QifException;
+import id.co.quadras.qif.core.QifUtil;
 import id.co.quadras.qif.core.model.entity.QifAdapter;
 import id.co.quadras.qif.core.model.entity.QifAdapterProperty;
-
-import java.util.List;
 
 /**
  * @author irwin Timestamp : 21/05/2014 18:38
@@ -18,17 +16,7 @@ public abstract class AbstractAdapter {
     }
 
     protected String getPropertyValue(String propertyKey) {
-        List<QifAdapterProperty> list = qifAdapter.getQifAdapterPropertyList();
-        String result = null;
-        if (list == null && list.isEmpty()) {
-            throw new QifException("FATAL : empty property list on QifAdapter " + qifAdapter.getName());
-        } else {
-            for (QifAdapterProperty property : qifAdapter.getQifAdapterPropertyList()) {
-                if (property.getPropertyKey().equals(propertyKey)) {
-                    result = property.getPropertyValue();
-                }
-            }
-        }
-        return result;
+        QifAdapterProperty qifAdapterProperty = QifUtil.getAdapterProperty(qifAdapter, propertyKey);
+        return (qifAdapterProperty == null) ? null : qifAdapterProperty.getPropertyValue();
     }
 }
